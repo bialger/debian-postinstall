f513f24702e81922eeaa1e7ed595476305549ed9
bialger <artur.bigulov@yandex.ru>
2024-01-31 19:05:07 +0300

    Updated scripts: added upgrade and user detecting

--- scripts/py/main.py
+++ scripts/py/main.py
@@ -6,4 +6,14 @@ import functions
 functions.check_for_error(platform.system() == "Linux", "This script can be started only on Linux")
 functions.check_for_error(functions.get_output("whoami") == "root", "Run this script only from root (use sudo)")
 
+print()
 print("Debian post-installation script started")
+
+logged = functions.get_output("lastlog").split("\n")[1:]
+users = [user_info.split()[0] for user_info in logged if user_info.split()[1][0:2] != "**"]
+
+print("Non root users:")
+
+for user in users:
+    if user != "root":
+        print(user)
--- start.sh
+++ start.sh
@@ -8,7 +8,14 @@ if [ "$(whoami)" != "root" ]; then
   exit 1
 fi
 
-python_path=$(echo $(find /usr/bin/python*) | cut -f 1 -d ' ')
+if [ -e /usr/bin/python ]; then
+  rm /usr/bin/python
+fi
+
+apt update > /dev/null 2> /dev/null
+apt upgrade -y
+apt install python3 python3-pip sudo -y
+python_path=$(echo $(find /usr/bin /bin -name "python3*" -not -name "*-*") | cut -f 1 -d ' ')
 ln "$python_path" /usr/bin/python
 chmod +x ./scripts/py/main.py
 ./scripts/py/main.py
